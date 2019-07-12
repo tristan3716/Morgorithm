d8cfe63c24f0f34ce5f2653e84811968ecafa551
Kim Hyeon-uk <tristan3716@gmail.com>
2019-07-12 09:53:22 +0900

    add source
    42860, 42898

---
+++ Week2/Programmers42860_Hyeon-uk.cpp
@@ -0,0 +1,271 @@
+/* ================================================================================================
+ * Programmers
+ *		42860 - 조이스틱
+ *
+ * time limit per test : ?? seconds
+ * memory limit per test : ?? megabytes
+ * solution function's arguments/return value
+ *
+ * 조이스틱으로 알파벳 이름을 완성하세요. 맨 처음엔 A로만 이루어져 있습니다.
+ * 
+ * 조이스틱을 각 방향으로 움직이면 아래와 같습니다.
+ * ▲ - 다음 알파벳
+ * ▼ - 이전 알파벳 (A에서 아래쪽으로 이동하면 Z로)
+ * ◀ - 커서를 왼쪽으로 이동 (첫 번째 위치에서 왼쪽으로 이동하면 마지막 문자에 커서)
+ * ▶ - 커서를 오른쪽으로 이동
+ * 만들고자 하는 이름 name이 매개변수로 주어질 때, 
+ * 이름에 대해 조이스틱 조작 횟수의 최솟값을 return 하도록 solution 함수를 만드세요.
+ *
+ * ------------------------------------------------------------------------------------------------
+ * 
+ * 알파벳을 맞출 때 위쪽 이동 횟수와 아래쪽 이동 횟수 중 더 작은 쪽을 택해서 이동
+ * 같은 방법으로 자리도 왼쪽과 오른쪽 중 더 짧은 방향으로 이동하면 되지 않을까?
+ *
+ * 구현 방법
+ *	A가 아닌 글자의 수 세기 : count
+ *	for()
+ *		현재 자리의 알파벳 설정 (A~Z) : count--
+ *		if (모든 자리의 알파벳 설정이 끝났는지) : count == 0
+ *		다음에 설정할 위치 탐색 (왼쪽과 오른쪽 중 짧은 곳)
+ *		이동
+ *
+ * 테스트케이스는 모두 통과하였으나...
+ * 만약 "FFFFFAAAAAAABA"라면?
+ * 당장 최선의 위치로 이동하는 것이 최종적으론 최선의 선택이 아닐 수도 있다
+ * 
+ * ...?
+ * 그리디인거 보고 고른건데...
+ * 
+ * 당장 생각난 해결 방법은 재귀로 모든 케이스에 대해 탐색하는 것.
+ * 왼쪽으로 이동했을 때 이동횟수와 오른쪽으로 이동했을 때 이동횟수의 합 중 더 작은 쪽을 선택
+ * 재귀 내부에서 방문한 자리에 대해 알파벳 설정 후 방문 표시하여 재방문을 방지
+ * 그리디가 적용되는 부분은 알파벳 설정
+ *
+ * 근데, 실제로 이동할 필요가 있나?
+ * 총 이동횟수 = 알파벳 설정 횟수의 합 + 이동 횟수의 합
+ * 알파벳 설정 횟수는 순서에 상관없이 불변.
+ * 즉, 이동 횟수만 최적화하면 됨
+ * 
+ * 이동할 필요가 없다면, 재귀호출할 필요도 없음.
+ * 문제를 푸는데 필요한건 각 자리에서 왼쪽, 오른쪽 중 더 가까운 위치까지의 거리
+ * 순차적으로 계산을 해서 더하자
+ *
+ * 안됨 ㅠ
+ * 양쪽으로 동시에 출발해서
+ * left로 가는 총 거리, right로 가는 총 거리를 구분하여
+ * 둘 중 더 짧은 쪽으로 유턴하면? min(left, right)*2+max(left, right)
+ *
+ * length/2까지 동시에 순회
+ * + 우측만, 좌측만
+ * 셋 중 가장 작은 값...
+ * 
+ * 이거 그리디 맞나?
+ * ------------------------------------------------------------------------------------------------ */
+
+
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int solution(string name) {
+	int name_length = name.size();
+
+	int vertical_up = 0;
+	int vertical_down = 0;
+
+	int total_move = 0;
+
+	int count = 0;
+	for (int i = 0; i < name_length; i++) {
+		if (name[i] != 'A')
+			count++;
+	}
+
+	for (int i = 0; i < name_length; i++){
+		//std::cout << name[i] << std::endl;
+		if (name[i] != 'A') {
+			vertical_up = name[i] - 'A';
+			vertical_down = (26 - vertical_up) % 26;
+			total_move += vertical_up < vertical_down ? vertical_up : vertical_down;
+		}
+
+		//if (i >= name_length - 1 || count == 0)
+		//	break;
+
+		//for (horizontal_pos_right = i + 1; name[horizontal_pos_right % name_length] == 'A'; horizontal_pos_right++);
+		//for (horizontal_pos_left = i - 1; name[(horizontal_pos_left + name_length) % name_length] == 'A'; horizontal_pos_left--);
+		//horizontal_pos_right = horizontal_pos_right % name_length;
+		//horizontal_pos_left = (horizontal_pos_left + name_length) % name_length;
+
+		//int right_dis = (horizontal_pos_right - i + name_length) % name_length;
+		//int left_dis = (i - horizontal_pos_left + name_length) % name_length;
+
+		//int distance;
+		//int direction = 0;
+		//if (right_dis <= left_dis) {
+		//	distance = right_dis;
+		//	direction = 1;
+		//}
+		//else {
+		//	distance = left_dis;
+		//	direction = 0;
+		//}
+
+		//if (name[i] != 'A' || (name[i] != 'A' && i==0 )) {
+		//	if (direction == 1) {
+		//		std::cout << "Add_r " << distance << std::endl;
+		//		total_move_r += distance;
+		//	}
+		//	else {
+		//		std::cout << "Add_l " << distance << std::endl;
+		//		total_move_l += distance;
+		//	} // A AABBBBB ABAAAAA
+		//	name[i] = 'A';
+		//	std::cout << "count--" << std::endl;
+		//	count--;
+		//}
+		//i += 1;
+	}
+
+	int right_only_move = 0;
+	int right_only_count = count;
+
+	int left_only_move = 0;
+	int left_only_count = count;
+
+	int turn_move = 0;
+	int turn_left_move = 0;
+	int turn_right_move = 0;
+
+	for (int i = 0; (right_only_count | left_only_count); i++) {
+		if (name[i] != 'A') {
+			right_only_count--;
+		}
+		if (right_only_count) {
+			right_only_move++;
+		}
+
+		if (left_only_count) {
+			left_only_move++;
+		}
+		if (name[name_length - i - 1] != 'A') {
+			left_only_count--;
+		}
+
+		if (i < name_length/2) {
+			if (i != 0) {
+				if (name[i] != 'A') {
+					turn_right_move = i;
+				}
+			}
+			if (name[name_length - i - 1] != 'A') {
+				turn_left_move = i + 1;
+			}
+		}
+	}
+	//std::cout << turn_left_move << std::endl;
+	//std::cout << turn_right_move << std::endl;
+	turn_move = turn_left_move < turn_right_move ? turn_left_move * 2 + turn_right_move : turn_left_move + turn_right_move * 2;
+
+	std::cout << turn_move << std::endl;
+	std::cout << left_only_move << std::endl;
+	std::cout << right_only_move << std::endl;
+	return total_move + std::min({turn_move, left_only_move, right_only_move});
+}
+
+#include <iostream>
+#include <thread>
+#include <tuple>
+#include <future>
+#include <chrono>
+#include <string>
+
+void get_solution(std::string name, std::promise<std::tuple<int, std::chrono::duration<double>>>* p)
+{
+	std::chrono::system_clock::time_point start = std::chrono::system_clock::now();
+	int result = solution(name);
+	std::chrono::system_clock::time_point end = std::chrono::system_clock::now();
+	std::chrono::duration<double> time = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+
+	p->set_value({ result, time });
+}
+
+int main()
+{
+	int right_ans_count = 0;
+	std::vector<std::tuple<std::string, int>> testcase;
+
+	/* 테스트케이스를 삽입하는 부분 */
+	testcase.push_back({ "JEROEN", 56 });
+	testcase.push_back({ "JAN", 23 });
+	testcase.push_back({ "ABAAAAAAAAABB", 7 });
+	testcase.push_back({ "AZAAAZ", 5 });
+	testcase.push_back({ "ABABAAAAAAAAAAAABA", 10 });
+	testcase.push_back({ "CANAAAAANAN", 48 });
+	testcase.push_back({ "BBABAAAAAAAAAAAABA", 11 });
+	testcase.push_back({ "BAAAAABA", 4 });
+	testcase.push_back({ "AAA", 0 });
+	testcase.push_back({ "", 0 });
+	testcase.push_back({ "AAAAAAAAAAAAAAAAAAAA", 0 });
+	testcase.push_back({ "ZZZZZZZZZZZZZZZZZZZZ", 39 });
+	testcase.push_back({ "Z", 1 });
+	testcase.push_back({ "AZAAAAAAAAZAZA", 9 });
+	testcase.push_back({ "ZZAAAAAAAAZAZA", 10 });
+	testcase.push_back({ "ZZAAAAAAAAZAZZ", 11 });
+	testcase.push_back({ "AZZZAAAAAAAZZZ", 15 });
+	testcase.push_back({ "ZZZZAAAAAAAZZZ", 16 });
+	testcase.push_back({ "AAAAZAAZAAA", 9 });
+
+	for (unsigned int i = 0; i < testcase.size(); i++) {
+		std::cout << "> Test No." << i + 1 << " ----------------------------------------" << std::endl;
+
+		std::string tc_name;
+		int tc_ans;
+		std::tie(tc_name, tc_ans) = testcase[i];
+
+
+		std::promise<std::tuple<int, std::chrono::duration<double>>> p;
+		auto f = p.get_future();
+
+		std::thread thr(get_solution, tc_name, &p);
+		std::future_status status = f.wait_for(std::chrono::milliseconds(2100));
+
+		if (status == std::future_status::timeout) {
+			thr.detach();
+
+			std::cout << "> Time Limit Exceed" << std::endl;
+		}
+		else if (status == std::future_status::ready) {
+			thr.join();
+
+			int answer = tc_ans;
+			std::chrono::duration<double> ex_time;
+			int result;
+			std::tie(result, ex_time) = f.get();
+
+			if (result != answer) {
+				std::cout << "  Check   : Wrong Answer" << std::endl;
+			}
+			else {
+				std::cout << "  Check   : Right Answer" << std::endl;
+				right_ans_count++;
+			}
+
+			std::cout << "  Ex_time : " << fixed << ex_time.count() << " seconds" << std::endl;
+
+			std::cout << "  Input   : " << tc_name << std::endl;
+
+			std::cout << "  Output  : " << result << std::endl;
+			std::cout << "  Answer  : " << answer << std::endl;
+		}
+
+		std::cout << std::endl;
+	}
+
+	std::cout << "> Correct " << right_ans_count << " of " << testcase.size() << std::endl;
+
+	return 0;
+}
---
+++ Week2/Programmers42898_Hyeon-uk.cpp
@@ -0,0 +1,134 @@
+/* ================================================================================================
+ * Programmers
+ *		42898 - 등굣길
+ *
+ * time limit per test : ?? seconds
+ * memory limit per test : ?? megabytes
+ * solution function's arguments/return value
+ *
+ * 집에서 학교까지 가는 길은 m x n 크기의 격자모양으로 나타낼 수 있습니다.
+ * 가장 왼쪽 위, 즉 집이 있는 곳의 좌표는 (1, 1)로 나타내고
+ * 가장 오른쪽 아래, 즉 학교가 있는 곳의 좌표는 (m, n)으로 나타냅니다.
+ * 격자의 크기 m, n과 물이 잠긴 지역의 좌표를 담은 2차원 배열 puddles이 매개변수로 주어질 때,
+ * 학교에서 집까지 갈 수 있는 최단경로의 개수를
+ * 1,000,000,007로 나눈 나머지를 return 하도록 solution 함수를 작성해주세요.
+ *
+ * 1 <= M, N <= 100 -> O((NM)^2) is possible
+ * m x n 격자에서 이동할 수 있는 경우의 수 : (m+n)!/m!n!
+ * P를 거치지 않고 갈 수 있는 경우의 수?
+ * P의 주위 4칸을 거쳐야함 -> P의 위와 오른쪽, P의 왼쪽과 아래
+ *
+ * Using DP
+ * puddle이 없을 때, map[0][0]부터 map[i][j]까지 도달할 수 있는 경로의 수
+ *     -> map[i-1][j], map[i][j-1] 까지 도달할 수 있는 경로의 수
+ * map[i][j]까지 도달할 수 있는 경로의 수에 메모이제이션 적용
+ *     -> map[i][j] = map[i][j-1] + map[i-1][j];
+ *
+ * puddle에 의해 막힌 곳을 경유하는 경로 = 0 으로 설정하면 (puddle에 도달하는 경로의 개수가 0)
+ * puddle을 경유하는 경로가 삭제됨
+ *
+ * ------------------------------------------------------------------------------------------------ */
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int m, int n, vector<vector<int>> puddles) {
+	vector<vector<int>> map(m + 1, vector<int>(n + 1, 0));
+	for (unsigned int i = 0; i < puddles.size(); i++) {
+		map[puddles[i][0]][puddles[i][1]] = -1;
+	}
+	map[0][1] = 1;
+	for (int i = 1; i <= m; i++) {
+		for (int j = 1; j <= n; j++) {
+			if (map[i][j] != -1)
+				map[i][j] = (map[i - 1][j] + map[i][j - 1]) % 1000000007;
+			else
+				map[i][j] = 0;
+		}
+	}
+	return map[m][n];
+}
+
+#include <iostream>
+#include <thread>
+#include <tuple>
+#include <future>
+#include <chrono>
+
+void get_solution(int m, int n, vector<vector<int>> puddles, std::promise<std::tuple<int, std::chrono::duration<double>>>* p)
+{
+	std::chrono::system_clock::time_point start = std::chrono::system_clock::now();
+	int result = solution(m, n, puddles);
+	std::chrono::system_clock::time_point end = std::chrono::system_clock::now();
+	std::chrono::duration<double> time = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+	
+	p->set_value({ result, time });
+}
+
+int main()
+{
+	/* 테스트케이스를 삽입하는 부분 */
+	std::vector<std::tuple<int, int, std::vector<std::vector<int>>, int>> testcase;
+
+	testcase.push_back({ 4, 3, { {2, 2} }, 4 });
+	testcase.push_back({ 6, 5, {  }, 30 });
+	testcase.push_back({ 1, 1, {  }, 1 });
+
+	for (unsigned int i = 0; i < testcase.size(); i++) {
+		std::cout << "> Test No." << i + 1 << std::endl;
+
+		int tc_m;
+		int tc_n;
+		std::vector<std::vector<int>> tc_puddles;
+		int tc_ans;
+		std::tie(tc_m, tc_n, tc_puddles, tc_ans) = testcase[i];
+
+
+		std::promise<std::tuple<int, std::chrono::duration<double>>> p;
+		auto f = p.get_future();
+
+		std::thread thr(get_solution, tc_m, tc_n, tc_puddles, &p);
+		std::future_status status = f.wait_for(std::chrono::milliseconds(2100));
+
+		if (status == std::future_status::timeout) {
+			thr.detach();
+
+			std::cout << "> Time Limit Exceed" << std::endl;
+		}
+		else if (status == std::future_status::ready) {
+			thr.join();
+
+			int answer = tc_ans;
+			std::chrono::duration<double> ex_time;
+			int result;
+			std::tie(result, ex_time) = f.get();
+
+			if (result != answer) {
+				std::cout << "  Check   : Wrong Answer" << std::endl;
+			}
+			else {
+				std::cout << "  Check   : Right Answer" << std::endl;
+			}
+
+			std::cout << "  Ex_time : " << fixed << ex_time.count() << " seconds" << std::endl;
+
+			std::cout << "  Input   : " << tc_m << ", " << tc_n << ", ";
+			std::cout << "[";
+			for (unsigned int i = 0; i < tc_puddles.size(); i++) {
+				std::cout << "[" << tc_puddles[i][0] << ", " << tc_puddles[i][1] << "]";
+				if (i != tc_puddles.size() - 1)
+					std::cout << ", ";
+			}
+			std::cout << "]" << std::endl;
+
+			std::cout << "  Output  : " << result << std::endl;
+			std::cout << "  Answer  : " << answer << std::endl;
+		}
+
+		std::cout << std::endl;
+	}
+
+	return 0;
+}
