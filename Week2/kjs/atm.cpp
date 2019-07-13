302ac9eb3fe10a35cb1a6a586e327f076bf6b338
kjs28 <48739864+kjs28@users.noreply.github.com>
2019-07-13 20:17:37 +0900

    주석 추가

--- Week2/kjs/atm.cpp
+++ Week2/kjs/atm.cpp
@@ -7,14 +7,14 @@ int get_sum(std::vector<int> v, int len);
 int main()
 {
 	int n;
-	std::cin >> n;
+	std::cin >> n;//인원수
 	std::vector<int> v(n);
 
 	for (auto i = v.begin(); i < v.end(); i++)
-		std::cin >> *i;
+		std::cin >> *i;//인원당 필요시간
 
-	sort(v.begin(), v.end());
-	int *arr = (int*)calloc(v.size(), sizeof(int));
+	sort(v.begin(), v.end());//최소 시간순으로 정렬
+	int *arr = (int*)calloc(v.size(), sizeof(int));//대기시간 포함 총 필요시간 저장할 배열
 	int res = 0;
 
 	for (int i=0;i<v.size();i++)
@@ -25,11 +25,11 @@ int main()
 		}
 		else
 		{
-			arr[i] = get_sum(v, i);
+			arr[i] = get_sum(v, i);//총 필요시간 저장
 		}
 	}
 	for (int i = 0; i < n; i++)
-		res = res + arr[i];
+		res = res + arr[i];//합산
 
 	std::cout << res;
 
@@ -42,8 +42,8 @@ int get_sum(std::vector<int> v, int len)
 	int res = 0;
 	for (; count < len+1; count++)
 	{
-		res = res + v[count];
+		res = res + v[count];//해당 인원까지의 소요시간+본인 사용시간
 	}
 
-	return res;
+	return res;//반환
 }
