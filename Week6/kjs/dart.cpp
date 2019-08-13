c45bdce00ee23b9470f4c7e6f107315993c010d6
kjs28 <48739864+kjs28@users.noreply.github.com>
2019-08-13 23:28:00 +0900

    .

---
+++ Week6/kjs/dart.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<algorithm>
+#include<string>
+#include<cstring>
+
+int solution(std::string dartResult)
+{
+	int answer = 0;
+}
+
+int main()
+{
+	std::string input="1S2D*3T";
+	int answer=solution(input);
+
+	std::cout << answer;
+
+	return 0;
+}
